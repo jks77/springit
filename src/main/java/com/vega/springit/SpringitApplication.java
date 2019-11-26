8b2f147f0b4f2e7ed4f6d34b08ed422f89bf06b6
jks77 <jankrijn@gmail.com>
2019-11-26 10:52:02 +0100

    Initial commit for our sping boot reddit clone

---
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -0,0 +1,14 @@
+package com.vega.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//dit runt de application
+@SpringBootApplication
+public class SpringitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitApplication.class, args);
+    }
+
+}
